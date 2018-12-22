1b81358873269b1c3de5212d10b8dec16511ff99
LordSk <lordskelethom@gmail.com>
2018-12-22 12:48:42 +0100

    Call update only when we render (ui mouse buttons can be wrong otherwise), setting up textinput

--- src/engine/client/client.cpp
+++ src/engine/client/client.cpp
@@ -2007,8 +2007,8 @@ void CClient::Run()
 
 			Update();
 
-			if(m_EditorActive)
-				m_pEditor->Update();
+			/*if(m_EditorActive)
+				m_pEditor->Update();*/
 
 			const bool SkipFrame = LimitFps();
 
@@ -2035,6 +2035,7 @@ void CClient::Run()
 						Render();
 					else
 					{
+						m_pEditor->Update();
 						m_pEditor->Render();
 						DebugRender();
 					}
