f885b198cbad8a25b7b4d05bc06943f844f4aaf5
Jordy Ruiz <jordy.ruiz@univ-lille.fr>
2019-01-20 11:23:33 +0100

    Upgrade to C++11

--- bam.lua
+++ bam.lua
@@ -78,7 +78,7 @@ end
 
 function GenerateCommonSettings(settings, conf, arch, compiler)
 	if compiler == "gcc" or compiler == "clang" then
-		settings.cc.flags:Add("-Wall", "-fno-exceptions")
+		settings.cc.flags:Add("-Wall", "-fno-exceptions", "-std=c++11")
 	end
 
 	-- Compile zlib if needed
